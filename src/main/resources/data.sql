a771efa3f50854f12ee419fbba9bd86301c25546
Dani <amaximoaiei.l@yahoo.com>
2022-06-07 15:17:23 +0300

    added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO USERS (ADDRESS, DEPARTMENTS, EMAIL, NAME, PASSWORD, PHONE_NUMBER, SEX, SURNAME, USER_TYPE, USERNAME)
+VALUES ('Mehedinti nr23', 'sonorizare', 'moholea58@gmail.com', 'Moholea', 'parolamea', '0751583188', 'M', 'Darius', 'member', 'Moholeaaa');
+
+INSERT INTO NOTIFICATIONS (MESSAGE, READ_DATE, SEND_DATE, SENDER, STATUS, TITLE, USER_ID)
+VALUES ('acesta este un mesaj', 'cand a fost citit', 'cand a fost trimis', 'Moholea', 'read', 'acesta este un titlu', 1);
